0ae68daddd05e5ce5ca2afc64c272d5953163048
graydon james <graydonj2@gmail.com>
2023-02-08 21:29:46 -0500

    Added colouring of fort items by how damaged they are

--- src/components/DisplayFort.js
+++ src/components/DisplayFort.js
@@ -1,6 +1,6 @@
 import DisplayFortSection from "./DisplayFortSection";
 
-function DisplayFort ({fights, fort, removeFortItem}) {
+function DisplayFort ({fights, fort, removeFortItem, baseFort}) {
   const fightText = (fights === 0) ? "No snowball fights yet!"
     : (fights === 1) ? `You have survived ${fights} snowball fight`
     : `You have survived ${fights} snowball fights`;
@@ -13,25 +13,28 @@ function DisplayFort ({fights, fort, removeFortItem}) {
         <div className="fort-section"><h4>WALLS</h4>
           {fort.map((fortItem) =>
             (fortItem.name === "wall") ? (
-              <DisplayFortSection key={fortItem.id} fortItem={fortItem} removeFortItem={removeFortItem}/>
+              <DisplayFortSection key={fortItem.id} fortItem={fortItem} removeFortItem={removeFortItem} baseHealth={baseFort[0].health}/>
               ) : null )}
         </div>
         <div className="fort-section"><h4>FORTIFICATIONS</h4>
           {fort.map((fortItem) =>
             (fortItem.name === "fortification") ? (
-              <DisplayFortSection key={fortItem.id} fortItem={fortItem} removeFortItem={removeFortItem} />
+              <DisplayFortSection key={fortItem.id} fortItem={fortItem} removeFortItem={removeFortItem} baseHealth={baseFort[2].health} />
             ) : null)}
         </div>
         <div className="fort-section"><h4>TOWERS</h4>
           {fort.map((fortItem) =>
             (fortItem.name === "tower") ? (
-              <DisplayFortSection key={fortItem.id} fortItem={fortItem} removeFortItem={removeFortItem} />
+              <DisplayFortSection key={fortItem.id} fortItem={fortItem} removeFortItem={removeFortItem} baseHealth={baseFort[1].health} />
             ) : null)}
         </div>
         <div className="fort-section"><h4>KEEPS</h4>
           {fort.map((fortItem) =>
             (fortItem.name === "keep") ? (
-              <div key={fortItem.id} className="fort-item-keep">
+              <div key={fortItem.id} className={
+                (fortItem.health > (0.6 * baseFort[3].health)) ? "fort-item-keep" :
+                (fortItem.health > (0.3 * baseFort[3].health)) ? "fort-item-keep fort-item-dmg" :
+                "fort-item-keep fort-item-xdmg"}>
                 <h5>{fortItem.name}</h5>
                 <button onClick={() => removeFortItem(fortItem)}>remove</button>
               </div>
